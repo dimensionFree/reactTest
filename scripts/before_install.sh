aae9eb41471d46232a0fcd0d557e077ee64bf959
freeLks <rinshouin666@gmail.com>
2024-09-26 02:06:38 +0900

    feat:add cicd files

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-echo "Stopping current Docker containers (if any)..."
-docker stop $(docker ps -q) || true
-docker rm $(docker ps -a -q) || true
+echo "Stopping and removing Docker containers containing the keyword 'frontend'..."
+
+# 停止名称中包含 'frontend' 的容器
+docker ps -q --filter "name=frontend" | xargs -r docker stop
+
+# 删除名称中包含 'frontend' 的容器
+docker ps -a -q --filter "name=frontend" | xargs -r docker rm
+
+echo "Containers with the keyword 'frontend' have been stopped and removed (if any)."
