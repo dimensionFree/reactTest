c367c1a2da99828a34afb6e53a6f0333f3ad2005
freeLks <rinshouin666@gmail.com>
2024-09-29 02:46:06 +0900

    feat:add cicd files

--- scripts/validate_service.sh
+++ scripts/validate_service.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 echo "Validating the service..."
 
-# 检查应用是否在 localhost:80 上运行
-echo "Checking if the application is running on localhost:8080..."
-until curl http://localhost:80; do
+# 检查应用是否在 localhost:443 上运行
+echo "Checking if the application is running on localhost:443..."
+until curl http://localhost:443; do
   echo "Application is not yet available. Waiting..."
   sleep 5
 done
 
-echo "Application is now running on localhost:80."
+echo "Application is now running on localhost:443."
