2857c27ba378996879fd1d66aa0830368e15ab4e
freeLks <rinshouin666@gmail.com>
2024-09-29 00:41:52 +0900

    feat:add cicd files

--- src/Utils/RequestSendUtils.js
+++ src/Utils/RequestSendUtils.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
-// const host = " http://localhost";
-const host = "ec2-52-197-155-24.ap-northeast-1.compute.amazonaws.com";
+// const host = "http://localhost";
+const host = "";
+// const host = "ec2-52-197-155-24.ap-northeast-1.compute.amazonaws.com";
 
 // const port = "8080";
 // ":"+port+
