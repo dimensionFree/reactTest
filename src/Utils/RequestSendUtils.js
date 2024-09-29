32bf9aeefe2ffc7f17c9e1fa2021fb0065894482
freeLks <rinshouin666@gmail.com>
2024-09-29 23:10:28 +0900

    feat:add cicd files

--- src/Utils/RequestSendUtils.js
+++ src/Utils/RequestSendUtils.js
@@ -1,11 +1,8 @@
 import axios from "axios";
 
-// const host = "http://localhost";
+// const host = "http://localhost:8080";
 const host = "";
-// const host = "ec2-52-197-155-24.ap-northeast-1.compute.amazonaws.com";
 
-// const port = "8080";
-// ":"+port+
 const hostAndPort=host+"/api"
 
 export default class RequestSendUtils {
